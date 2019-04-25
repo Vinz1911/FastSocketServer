1067e61c5b21dee72c7d0bcdff36d196709a001d
Vinzenz Weist <vinzenz.weist@telekom.de>
2019-04-26 00:37:21 +0200

    [M] finished example, updated readme

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
 # FastSocketServer
-This is the server implementation of the FastSocket Protocol.
 
-This is a custom package!
+## About:
+FastSocketServer is a golang package which implements the `FastSocket Protocol`.
+The FastSocket Protocol is a message based tcp communication protocol, it is probably the 
+fastest message based communication protocol with the lowest overhead.
+ 
+## Install:
+```
+go get github.com/vinz1911/fastsocketserver
+```
+
+## Usage:
+```go
+package main
+
+import (
+"net"
+
+"github.com/vinz1911/fastsocketserver"
+)
+
+func main() {
+server := fastsocket.Server{}
+server.Start(8081)
+}
+```
+
+## Example:
+Go into the example folder to see a example implementation of the FastSocket Server.
+I used it to create a `Speedtest Backend`
--- example/server.go
+++ example/server.go
@@ -1,17 +1,45 @@
 package main
 
-import (
-	"log"
-	"net"
+// Example of a `Speedtest Server` implementation with
+// the help of the FastSocket Protocol
 
-	"github.com/Vinz1911/fastsocket"
-	//"github.com/vinz1911/fastsocket"
+import (
+	"net"
+	"strconv"
+
+	"github.com/vinz1911/fastsocketserver"
 )
 
+// Mapper is a Helper to convert datatypes
+type Mapper struct{}
+
 func main() {
-	var socket = fastsocket.Server{}
-	socket.OnBinaryMessage = func(str []byte, socket net.Conn) {
+	port := uint16(8081)
+	mapper := Mapper{}
+	server := fastsocket.Server{}
+	server.OnBinaryMessage = func(data []byte, socket net.Conn) {
+		size := len(data)
+		message := mapper.intToStr(size)
+		server.SendString(message, socket)
 	}
-	socket.Start(3333)
-	log.Println("Hello World")
+	server.OnTextMessage = func(str string, socket net.Conn) {
+		size := str
+		message := make([]byte, mapper.strToInt(size))
+		server.SendData(message, socket)
+	}
+	server.Start(port)
+}
+
+// convert a string to an integer
+func (*Mapper) strToInt(str string) int {
+	value, err := strconv.Atoi(str)
+	if err != nil {
+	}
+	return value
+}
+
+// convert an integer to a string
+func (*Mapper) intToStr(integer int) string {
+	value := strconv.Itoa(integer)
+	return value
 }

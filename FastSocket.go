f9e55ec24e4c8ad5ca89ee672ff1cf2f7c30bbb6
Vinzenz Weist <Vinz1911@gmail.com>
2019-05-02 21:40:47 +0200

    [F] added maximum content length

--- FastSocket.go
+++ FastSocket.go
@@ -10,11 +10,14 @@ import (
 // Server represents the implementation of
 // the FastSocket Protocol (Server sided)
 type Server struct {
+	// write lock
+	mutexLock bool
+	// socket
 	socket net.Listener
 	// Closure for incoming text messages
-	OnTextMessage StringClosureSocket
+	OnTextMessage stringClosureSocket
 	// Closure for incoming data messages
-	OnBinaryMessage ByteClosureSocket
+	OnBinaryMessage byteClosureSocket
 }
 
 // Frame is a struct to Create and parse the
@@ -27,68 +30,66 @@ type Server struct {
 // |   O P C O D E   |         Payload Data...      |  F I N B Y T E  |
 // +-----------------+------------------------------+-----------------+
 //
-type Frame struct {
+type frame struct {
 	cache    []byte
-	onText   ByteClosure
-	onBinary ByteClosure
+	onText   byteClosure
+	onBinary byteClosure
 }
 
-// Mapper is a Helper to convert Datatypes
-type Mapper struct{}
+// ByteClosureSocket Descripes a closure which returns a byte array
+type byteClosureSocket func([]byte, net.Conn)
 
-// ByteClosureSocket Descripes a clsoure which returns a byte array
-type ByteClosureSocket func([]byte, net.Conn)
+// StringClosureSocket Descripes a closure which returns a string
+type stringClosureSocket func(string, net.Conn)
 
-// StringClosureSocket Descripes a clsoure which returns a string
-type StringClosureSocket func(string, net.Conn)
+// ByteClosure Descripes a closure which returns a byte array
+type byteClosure func([]byte)
 
-// ByteClosure Descripes a clsoure which returns a byte array
-type ByteClosure func([]byte)
-
-// StringClosure Descripes a clsoure which returns a string
-type StringClosure func(string)
+// StringClosure Descripes a closure which returns a string
+type stringClosure func(string)
 
 // ControlCode are the Codes which
 // describe the fragmented frame
-type ControlCode uint8
+type controlCode uint8
 
 // Opcode are Status Codes for
 // the Message
-type Opcode uint8
+type opcode uint8
 
 const (
 	// continueByte is a placeholder
-	continueByte ControlCode = 0x0
+	continueByte controlCode = 0x0
 	// text holds the byte for a text message
-	text Opcode = 0x1
+	text opcode = 0x1
 	// binary holds the byte for a binary message
-	binary Opcode = 0x2
+	binary opcode = 0x2
 	// finByte holds the ControlCode for `end of a message`
-	finByte ControlCode = 0x03
+	finByte controlCode = 0x03
 	// acceptByte is for ACK if the handshake succeed
-	acceptByte ControlCode = 0x06
+	acceptByte controlCode = 0x06
 	// connectionClose holds the byte for convenient closed connections (not used now)
-	connectionClose Opcode = 0x8
+	connectionClose opcode = 0x8
 	// socketID is the unique ID to identify protocol conformance connections (used for the handshake)
 	socketID string = "6D8EDFD9-541C-4391-9171-AD519876B32E"
 	// maximumLength is the maximum buffer read length
 	maximumLength int = 8192
+	// maximum frame size
+	maximumContentLength int = 16777216
 )
 
 // Start starts the FastSocketServer and handles all incoming connection
 func (server *Server) Start(port uint16) {
-	mapper := Mapper{}
 	err := errors.New("")
-	server.socket, err = net.Listen("tcp", ":"+mapper.intToStr(int(port)))
+	server.socket, err = net.Listen("tcp", ":"+strconv.Itoa(int(port)))
 	if err != nil {
 		log.Println(err)
 	}
-	log.Println("FastSocket Server started on Port:", mapper.intToStr(int(port)))
+	log.Println("FastSocket Server started on Port:", strconv.Itoa(int(port)))
 	defer server.socket.Close()
 	for {
 		connection, err := server.socket.Accept()
 		if err != nil {
-			log.Panicln(err)
+			log.Println(err)
 		}
 		go server.loop(connection)
 	}
@@ -99,63 +100,82 @@ func (server *Server) Stop() {
 	server.socket.Close()
 }
 
-func (server *Server) loop(socket net.Conn) {
-	locked := false
-	frame := Frame{}
-	server.frameClosures(&frame, socket)
+// SendData is used to send a binary message to the client
+func (server *Server) SendData(data []byte, conn net.Conn) {
+	frame := frame{}
+	message := frame.create(&data, binary)
+	server.write(message, conn)
+}
+
+// SendString is used to send a text message to the client
+func (server *Server) SendString(str string, conn net.Conn) {
+	frame := frame{}
+	data := []byte(str)
+	message := frame.create(&data, text)
+	server.write(message, conn)
+}
+
+func (server *Server) loop(conn net.Conn) {
+	mutexLock := false
+	frame := frame{}
+	server.frameClosures(&frame, conn)
 	for {
 		buffer := make([]byte, maximumLength)
-		size, err := socket.Read(buffer)
+		size, err := conn.Read(buffer)
 		if err != nil {
 			return
 		}
 		data := buffer[:size]
-		switch locked {
+		switch mutexLock {
 		case true:
-			frame.parse(&data)
+			err := frame.parse(&data)
+			if err != nil {
+				log.Println(err)
+				conn.Close()
+				return
+			}
 		case false:
 			if string(data) == socketID {
-				locked = true
-				socket.Write([]byte{byte(acceptByte)})
+				mutexLock = true
+				conn.Write([]byte{byte(acceptByte)})
 			} else {
-				socket.Close()
+				conn.Close()
 			}
 		}
 	}
 }
 
+// internal sending function
+func (server *Server) write(data *[]byte, conn net.Conn) {
+	if server.mutexLock {
+		return
+	}
+	server.mutexLock = true
+	_, err := conn.Write(*data)
+	if err != nil {
+		log.Println(err)
+		server.mutexLock = false
+	} else {
+		server.mutexLock = false
+	}
+}
+
 // Handle Response for Speedtest
-func (server *Server) frameClosures(frame *Frame, socket net.Conn) {
+func (server *Server) frameClosures(frame *frame, conn net.Conn) {
 	frame.onText = func(data []byte) {
 		message := string(data)
-		server.OnTextMessage(message, socket)
+		server.OnTextMessage(message, conn)
 	}
 	frame.onBinary = func(data []byte) {
-		server.OnBinaryMessage(data, socket)
+		server.OnBinaryMessage(data, conn)
 	}
-}
-
-// SendData is used to send a binary message to the client
-func (*Server) SendData(data []byte, socket net.Conn) {
-	frame := Frame{}
-	byted := data
-	message := frame.create(&byted, binary)
-	socket.Write(*message)
-}
-
-// SendString is used to send a text message to the client
-func (*Server) SendString(str string, socket net.Conn) {
-	frame := Frame{}
-	byted := []byte(str)
-	message := frame.create(&byted, text)
-	socket.Write(*message)
 }
 
 // Create a FastSocket Protocol compliant frame
 // this functions add the neccessary bytes to the buffer
-func (*Frame) create(data *[]byte, opcode Opcode) *[]byte {
+func (*frame) create(data *[]byte, op opcode) *[]byte {
 	buffer := []byte{}
-	buffer = append(buffer, byte(opcode))
+	buffer = append(buffer, byte(op))
 	buffer = append(buffer, *data...)
 	buffer = append(buffer, byte(finByte))
 	return &buffer
@@ -163,46 +183,42 @@ func (*Frame) create(data *[]byte, opcode Opcode) *[]byte {
 
 // Parse received Data into a FastSocket compliant
 // frame/message
-func (frame *Frame) parse(data *[]byte) {
+func (frame *frame) parse(data *[]byte) error {
 	if len(*data) <= 0 {
-		return
+		return errors.New("zero data")
 	}
 	frame.cache = append(frame.cache, *data...)
+	if len(frame.cache) > maximumContentLength {
+		return errors.New("readbuffer overflow")
+	}
 	if (*data)[len(*data)-1] == byte(finByte) {
 		if frame.cache[0] == byte(text) {
-			var message = frame.trimmedFrame(frame.cache)
+			message, err := frame.trimmedFrame(frame.cache)
+			if err != nil {
+				return err
+			}
 			frame.onText(message)
 		}
 		if frame.cache[0] == byte(binary) {
-			var message = frame.trimmedFrame(frame.cache)
+			message, err := frame.trimmedFrame(frame.cache)
+			if err != nil {
+				return err
+			}
 			frame.onBinary(message)
 		}
 		frame.cache = []byte{}
 	}
+	return nil
 }
 
 // helper function to trimm a frame into a message
-func (*Frame) trimmedFrame(data []byte) []byte {
+func (*frame) trimmedFrame(data []byte) ([]byte, error) {
 	var trimmed = data
 	if len(data) >= 3 {
 		_, trimmed = trimmed[0], trimmed[1:]
 		trimmed = trimmed[:len(trimmed)-1]
 	} else {
-		log.Println(errors.New("Cannot parse Frame"))
+		return nil, errors.New("cannot parse Frame")
 	}
-	return trimmed
-}
-
-// convert a string to an integer
-func (*Mapper) strToInt(str string) int {
-	value, err := strconv.Atoi(str)
-	if err != nil {
-	}
-	return value
-}
-
-// convert an integer to a string
-func (*Mapper) intToStr(integer int) string {
-	value := strconv.Itoa(integer)
-	return value
+	return trimmed, nil
 }
